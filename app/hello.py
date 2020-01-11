4aab0824f95dcd39ace57dcfc6bd27a041934db9
erolkeskiner <erolksknr@gmail.com>
2020-01-11 21:09:14 +0300

    minor

--- app/hello.py
+++ app/hello.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!
+    return 'Hello, World!'
 
 @app.route('/test')
 def hello_test():
-    return 'Hello, Test!''
+    return 'Hello, Test!'
