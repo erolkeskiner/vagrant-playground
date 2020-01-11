f0c5228939f4ce7295282872f19e6f4cdcf11845
erolkeskiner <erolksknr@gmail.com>
2020-01-11 20:27:17 +0300

    provision flask env and start the app

---
+++ deploy/bootstrap.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get install -y git-core python-pip python-virtualenv nginx
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+ssh -T git@github.com
+git clone https://$GITHUB_API_TOKEN@github.com/erolkeskiner/vagrant-playground.git
+cd vagrant-playground
+pip install -r requirements.txt
+cd app/
+export FLASK_APP=hello.py
+flask run --host=0.0.0.0 
